bbedc8d6f6f4a5e3259ec1e65ce43046d0978cd0
Jonathan Plata <jonathan_plata@ymail.com>
2018-01-16 23:02:56 -0800

    rules route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import './App.css';
-
-import Layout from './containers/Layout';
+// import Layout from './containers/Layout';
+import LandingPage from'./components/LandingPage';
+import Rules from './components/Rules';
 
 class App extends Component {
   render () {
     return (
-      <div>
-        
-          <Layout />
-      
-      </div>
+      <BrowserRouter>
+      	<Switch>
+          {/*<Route exact path="/layout" component={Layout}/>  */}
+          <Route exact path="/rules" component={Rules}/>
+      		<Route exact path="/" component={LandingPage}/>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class LandingPage extends Component {
+	render() {
+		return (
+			<div>
+				<h3>Welcome</h3>
+				<p>test test</p>
+			</div>
+		)
+	}
+}
+
+export default LandingPage;
