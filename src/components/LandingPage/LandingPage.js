3a90a2c6cbbf9ee75d60b2d7f91246d38bcff024
Jonathan Plata <jonathan_plata@ymail.com>
2018-01-17 11:19:36 -0800

    setup

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,11 +1,49 @@
 import React, {Component} from "react";
+import './LandingPage.css';
 
 class LandingPage extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+		  btnClick: false,
+		  name: ''
+		}
+
+		this.createLobby = this.createLobby.bind(this);
+		this.joinGame = this.joinGame.bind(this);
+	}
+
+	createLobby() {
+		this.setState({btnClick: true});
+	}
+
+	joinGame() {
+		this.setState({btnClick: true});
+		console.log(this.state);
+	}
+
 	render() {
 		return (
 			<div>
-				<h3>Welcome</h3>
-				<p>test test</p>
+				<div className="header">
+					<h2>Game Header</h2>
+				</div>
+				<br/><br/>
+				<div className="content">
+					<button className="create" onClick={this.createLobby}><b>Create Lobby</b></button>
+					<button className="join" onClick={this.joinGame}><b>Join Existing Game</b></button>
+				</div>
+				<br/>
+				{this.state.btnClick ?
+					<div>
+						<form>
+							<input></input>
+
+						</form>
+					</div>
+					: <p>make selection</p>
+				}
 			</div>
 		)
 	}
