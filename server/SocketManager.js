0402edff088b02fb4e11a84037d9d216bb70eaa8
Jonathan Plata <jonathan_plata@ymail.com>
2018-01-18 20:58:28 -0800

    change

--- server/SocketManager.js
+++ server/SocketManager.js
@@ -2,7 +2,6 @@ const io = require('./index.js').io;
 
 const { VERIFY_USER, USER_CONNECTED, USER_DISCONNECTED, LOGOUT, LETTER_UPDATE, WORD_CHALLENGED, PLAYER_UNSUCCESSFUL, PLAYER_SUCCESSFUL, YOUR_TURN, SEND_MODAL, NEW_ROOM } = require('../src/Events');
 const MAX_WAITING = 5000;
-const { createUser } = require('../Factories');
 
 const sessions = [];
 
