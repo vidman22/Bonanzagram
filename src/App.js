a51d1d38e7ac8e88e850619f89bfc1f8a129be3a
vidman22 <jvidmar37@gmail.com>
2018-01-20 15:35:48 -0800

    cleaner imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
-// import Layout from './containers/Layout';
 import LandingPage from'./containers/LandingPage.js';
-// import Rules from './containers/Rules';
-import Lobby from './components/Lobby';
-import Test from './components/TestComp/testComponent.js';
-import Layout from './containers/Layout/Layout.js';
+
 
  
 class App extends Component {
