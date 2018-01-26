d4f4135126122f5e107f8680c0e5c22516340311
vidman22 <jvidmar37@gmail.com>
2018-01-25 19:14:59 -0800

    css

--- src/containers/Rules/Rules.js
+++ src/containers/Rules/Rules.js
@@ -15,13 +15,13 @@ class Rules extends Component {
 				<br/>
 				<div className="content">
 		
-					<p>The goal of the game is to continue spelling a word without any errors and without ending a word.</p>
+					<p>	   The goal of the game is to continue spelling a word without any errors and without ending a word.</p>
 					<p>If you complete a word, then the next player can declare your word completed and you will be docked points 
 					equivalent to the number of letters in the word. If they prematurely declared your word compelted, they will 
 					be docked the points.</p>
-					<p>If you spell a word incorrectly, the subsequent player can challenge your spelling and you must prove 
+					<p>    If you spell a word incorrectly, the subsequent player can challenge your spelling and you must prove 
 					that you were spelling an actual word.</p>
-					<p>If you've successfully entered the word you had been spelling, the challenger will be docked points equivalent to the number of 
+					<p>    If you've successfully entered the word you had been spelling, the challenger will be docked points equivalent to the number of 
 					letters in the word. If your word was incorrect, then you will lose the points.</p>
 					
 					<p>Happy Spelling!</p>
